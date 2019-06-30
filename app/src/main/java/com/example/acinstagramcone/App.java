e0dc3697eb56f1df88d1c2472f4eea4a0700145a
vinaygoyal355 <vg83680@gmail.com>
2019-06-30 13:48:45 +0530

    Initial commit

---
+++ app/src/main/java/com/example/acinstagramcone/App.java
@@ -0,0 +1,19 @@
+package com.example.acinstagramcone;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId(getString(R.string.back4app_app_id))
+                .clientKey(getString(R.string.back4app_client_Key))
+                .server(getString(R.string.back4app_server_url)).build());
+
+    }
+}
