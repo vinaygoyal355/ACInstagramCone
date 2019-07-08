62b56d93a5fd1caf159126702f0880f49d0a01bd
vinaygoyal355 <vg83680@gmail.com>
2019-07-08 12:28:50 +0530

    Fancy Toast

--- app/src/main/java/com/example/acinstagramcone/SignUp.java
+++ app/src/main/java/com/example/acinstagramcone/SignUp.java
@@ -3,45 +3,66 @@ package com.example.acinstagramcone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.SaveCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
-public class SignUp extends AppCompatActivity {
+public class SignUp extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnSave;
+    private EditText edtKickSpeed,edtKickPower,edtName,edtPunchPower,edtPunchSpeed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    public void hello_worldTapped(View view){
-    /*    ParseObject boxer=new ParseObject("Boxer");
-        boxer.put("punch_speed",200);
-        boxer.saveInBackground(new SaveCallback() {
-            @Override
-            public void done(ParseException e) {
-                if(e==null){
-                    Toast.makeText(SignUp.this,"Boxer Object is saved successfully",Toast.LENGTH_LONG).show();
-                }
-            }
-        });*/
 
-        ParseObject KickBoxer=new ParseObject("KickBoxer");
-        KickBoxer.put("Name","Vinay");
-        KickBoxer.put("KickSpeed",100);
-        KickBoxer.put("KickPower",200);
-        KickBoxer.put("PunchSpeed",300);
-        KickBoxer.put("PunchPower",400);
-        KickBoxer.saveInBackground(new SaveCallback() {
-            @Override
-            public void done(ParseException e) {
-                if(e == null){
-                    Toast.makeText(SignUp.this,"KickBoxer is saved successfully",Toast.LENGTH_LONG).show();
+        btnSave=findViewById(R.id.btnSave);
+        btnSave.setOnClickListener(this);
+
+        edtName=findViewById(R.id.edtName);
+        edtKickPower=findViewById(R.id.edtKickPower);
+        edtKickSpeed=findViewById(R.id.edtKickSpeed);
+        edtPunchPower=findViewById(R.id.edtPunchPower);
+        edtPunchSpeed=findViewById(R.id.edtPunchSpeed);
+    }
+
+    @Override
+    public void onClick(View view) {
+            final ParseObject KickBoxer=new ParseObject("KickBoxer");
+
+            String a;
+            int b,c,d,e;
+            a=edtName.getText().toString();
+            b=Integer.parseInt(edtKickSpeed.getText().toString());
+            c=Integer.parseInt(edtKickPower.getText().toString());
+            d=Integer.parseInt(edtPunchSpeed.getText().toString());
+            e=Integer.parseInt(edtPunchPower.getText().toString());
+
+            Log.i("Punch","Running");
+
+            KickBoxer.put("Name",a);
+            KickBoxer.put("KickSpeed",b);
+            KickBoxer.put("PunchPower",e);
+            KickBoxer.put("KickPower",c);
+            KickBoxer.put("PunchSpeed",d);
+            KickBoxer.saveInBackground(new SaveCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if(e == null){
+                        FancyToast.makeText(SignUp.this,"Hello World !",FancyToast.LENGTH_LONG,FancyToast.SUCCESS,true);
+                    }
+                    else{
+                        Toast.makeText(SignUp.this,e.getMessage(),Toast.LENGTH_LONG).show();
+                    }
                 }
-            }
-        });
+            });
     }
 }
